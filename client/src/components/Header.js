93657f837c38830442f6592a611213ac00899620
jbelciglio <jbelciglio@gmail.com>
2018-10-30 10:59:56 -0700

    ride my bike with no handlebars

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,20 +2,50 @@ import React from "react";
 import {Link} from "react-router-dom";
 import "./Header.css";
 
+var navbarStyle = {
+  display: "flex",
+  justifyContent: "space-around",
+  flexFlow: "row wrap",
+  float: "right"
+}
+var headerStyle = {
+  width: "100%",
+  fontColor: "white"
+}
+var linkStyle = {
+  paddingLeft: "10px",
+  paddingRight: "10px"
+}
+
+var h1Style = {
+  paddingRight: "15px",
+  color: "white"
+}
+
+var navStyle = {
+  width: "100px"
+}
+
+
 const Header = () => {
   return (
-    <header className="mb-4">
-      <h1 className = "header">Code Snippet</h1>
+    <nav className="navbar navbar-dark bg-none">
+    <header className="row" style={headerStyle}>
+      <h1 style={h1Style}><Link to="/" className="nav-link" style={linkStyle}>bitHub</Link></h1>
+      <div className="navbar-nav" style={navbarStyle}>
+      
+      <Link to="/resources" className="nav-link" style={linkStyle}>Resources</Link>
+      
+      <Link to="/search" className="nav-link" style={linkStyle}>Search</Link>
+      
+      <Link to="/new" className="nav-link" style={linkStyle}>New Snippet</Link>
+      </div>
+      </header>
+      </nav>
+  )}
 
-      <Link to="/" className="badge badge-primary">Home</Link>
-      {" "}
-      <Link to="/resources" className="badge badge-primary">Resources</Link>
-      {" "}
-      <Link to="/search" className="badge badge-primary">Search</Link>
-      {" "}
-      <Link to="/new" className="badge badge-primary">New Snippet</Link>
-    </header>
-  );
-};
+
+
+
 
 export default Header;
