14df93030cc4f68b5b64fa487290779709ad6de7
jbelciglio <jbelciglio@gmail.com>
2018-10-31 10:12:05 -0700

    mission statement

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -15,8 +15,12 @@ class Home extends React.Component {
     return (
       <div>
         
-        <div>
-            <h1 className = "currentPage"> This is the Home page. </h1>
+        <div className="jumbotron">
+            <h1 className = "currentPage"><span className="blue">Created</span> <span className="purple">For</span> (<span className="developer">Developers</span>) <span className="purple">By</span> [<span className="developer">Developers</span>];</h1>
+            <br></br>
+            <h3 className = "currentPage">bitHub is the latest tool for developers to share knowledge with their community. 
+            Here, you can save your best code for all aspiring developers to use and learn from. 
+            You will become an integral part in both the enriching and the growth of developers everywhere.</h3>
         </div>
       </div>
     );
