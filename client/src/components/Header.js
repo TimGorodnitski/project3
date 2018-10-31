e7d4617bb8bcc72506db884ceed556b64147c9c0
Kyle Schoch <kgschoch@gmail.com>
2018-10-31 14:10:26 -0700

    navbar styling

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -13,26 +13,26 @@ var headerStyle = {
   fontColor: "white"
 }
 var linkStyle = {
-  paddingLeft: "10px",
-  paddingRight: "10px",
-  color: "lightblue",
-  fontSize: "25px",
+  paddingLeft: "15px",
+  paddingRight: "15px",
+  color: "rgb(223, 88, 223)",
+  fontSize: "27px",
   opacity: "0.7"
 }
 
 var linkstyle = {
-  paddingLeft: "10px",
-  paddingRight: "10px",
+  paddingLeft: "15px",
+  paddingRight: "15px",
   color: "lightgreen",
-  fontSize: "25px",
+  fontSize: "27px",
   opacity: "0.7"
 }
 
 var LinkStyle = {
-  paddingLeft: "10px",
-  paddingRight: "10px",
+  paddingLeft: "15px",
+  paddingRight: "15px",
   color: "orange",
-  fontSize: "25px",
+  fontSize: "27px",
   opacity: "0.7"
 }
 
@@ -48,8 +48,25 @@ var navStyle = {
 var logo = {
   width: "75px",
   height: "75px",
-  opacity: "0.5",
+  opacity: "0.9",
+  marginTop: "2px",
+  padding: "5px",
+  cursor: "pointer",
+  backgroundColor: "rgba(25, 25, 25, 0)",
+  borderRadius: "50%",
   
+}
+
+var loginStyle = {
+  height: "50px",
+  width: "50px",
+  backgroundColor: "grey",
+  marginLeft: "20px",
+  marginTop: "15px",
+  marginBottom: "15px",
+  borderRadius: "50%",
+  cursor: "pointer",
+  border: "2px solid black"
 
 }
 
@@ -59,7 +76,9 @@ const Header = () => {
     <div>
     <nav className="navbar navbar-dark bg-none">
     <header className="row" style={headerStyle}>
-    <img className="logoIMG" style={logo} src = "https://t4.ftcdn.net/jpg/01/83/14/31/240_F_183143127_vT6GQhv9GxxIfk4ZE6QennKjGssUlK1X.jpg"></img>
+
+    <img className="logoIMG" style={logo} src = "https://png.pngtree.com/png_detail/18/09/10/pngtree-Initial-Letter-BH-Logo-Design-png-clipart_3579590.jpg"></img>
+
       <h1 style={h1Style}><Link to="/" className="nav-link">BitHub</Link></h1>
       <div className="navbar-nav" style={navbarStyle}>
       
@@ -68,6 +87,8 @@ const Header = () => {
       <Link to="/search" className="nav-link navLink" style={linkstyle}>Search</Link>
       
       <Link to="/new" className="nav-link navLink" style={LinkStyle}>New Snippet</Link>
+      <img className = "loginIMG" style={loginStyle} src="https://static.thenounproject.com/png/23665-200.png"></img>
+
       </div>
       </header>
       </nav>
