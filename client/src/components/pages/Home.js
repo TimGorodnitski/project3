fd342e4206416f4b0ce0106a39e9064ec3c44924
jbelciglio <jbelciglio@gmail.com>
2018-10-31 20:14:18 -0700

    reworked jumbotron

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -130,15 +130,18 @@ class Home extends React.Component {
         <div className="container">
             
             <button id="openModal" onClick={this.toggleSignInModal}>Sign In</button>
-      
-        <div className="jumbotron">
-            <h2 className = "currentPage">1 <span className="blue">Created</span> <span className="purple">For
-            </span> (<span className="developer">Developers</span>) <span className="purple">By</span> [<span className="developer">Developers</span>] <span className="blue">=></span> &#123;</h2>
+      <div className="row">
+        <div className="col-md-7">
+        <div className="jumbotron shadow-lg  p-3 mb-5">
+            <h5 className = "currentPage">1 <span className="blue">Created</span> <span className="purple">For
+            </span> (<span className="developer">Developers</span>) <span className="purple">By</span> [<span className="developer">Developers</span>] <span className="blue">=></span> &#123;</h5>
             {/*first sentence*/}
-            <h3 className = "currentPage">2 <br></br>3 &#60;<span className="red">BitHub</span> <span className="yellow">is</span> the latest <span className="blue">tool</span> <span className="purple">for</span> <span className="developer">developers</span> to <span className="blue">spread</span><br></br>4 <span className="lightBlue">knowledge</span> within their <span className="lightBlue">community</span>/&#62;; <br></br>5 <br></br>{/*second sentence*/}
+            <h6 className = "currentPage">2 <br></br>3 &#60;<span className="red">BitHub</span> <span className="yellow">is</span> the latest <span className="blue">tool</span> <span className="purple">for</span> <span className="developer">developers</span> to <span className="blue">spread</span><br></br>4 <span className="lightBlue">knowledge</span> within their <span className="lightBlue">community</span>/&#62;; <br></br>5 <br></br>{/*second sentence*/}
             6 &#60;<span className="blue">Here</span>, you can <span className="yellow">save</span> and <span className="yellow">share</span> your <span className="blue">best</span> <span className="lightBlue">code</span> <span className="purple">for</span> all <span className="purple">aspiring</span> <br></br>7 <span className="developer">developers</span> across the <span className="lightBlue">globe</span>/&#62;;<br></br>8 <br></br>{/*third sentence*/}
-            9 &#60;You will <span className="yellow">become</span> an <span className="lightBlue">integral</span> part in both the <br></br>10 <span className="purple">enrichment</span> and the <span className="purple">growth</span> of <span className="developer">developers</span> everywhere/&#62;;<br></br>11 <br></br>12 &#125;;</h3>
+            9 &#60;You will <span className="yellow">become</span> an <span className="lightBlue">integral</span> part in both the <br></br>10 <span className="purple">enrichment</span> and the <span className="purple">growth</span> of <span className="developer">developers</span> everywhere/&#62;;<br></br>11 <br></br>12 &#125;;</h6>
         </div>
+      </div>
+    </div>
         <div id="modal" style={this.state.currentSignInModalStyle}>
           <button id="closeModal" onClick={this.toggleSignInModal}>x</button>
           <h1>Sign In</h1>
