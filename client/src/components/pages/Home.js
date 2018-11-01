68832e5afe94a96314c2269fda0a051d645c8091
Tim Gorodnitski <timgm3@gmail.com>
2018-11-01 12:08:54 -0700

    login working, again

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -3,7 +3,6 @@ import "./Home.css";
 // import {Link} from "react-router-dom";
 import axios from "axios";
 
-
 class Home extends React.Component {
   state = {
     styles1: {
@@ -13,75 +12,48 @@ class Home extends React.Component {
       display: "none"
     },
     username: "",
-    password: "",
-    signInModalOpen: false,
-    currentSignInModalStyle: this.closedModalStyle,
-    signUpModalOpen: false,
-    currentSignUpModalStyle: this.closedModalStyle
-
-
+    password: ""
   };
-
-  closedModalStyle = {
-    display: "none",  /* Hidden by default */
-    position: "fixed",  /* Stay in place */
-    zIndex: 1,  /* Sit on top */
-    top: 0,
-    width: "100%", /* Full width */
-    height: "100%",  /* Full height */
-    overflow: "auto",  /* Enable scroll if needed */
-    backgroundColor: "rgb(0,0,0)",  /* Fallback color */
-    backgroundColor: "rgba(0,0,0,0.4)",  /* Black w/ opacity */
-  }
-
-  openedModalStyle = {
-    display: "block",  /* Hidden by default */
-    position: "fixed",  /* Stay in place */
-    zIndex: 1,  /* Sit on top */
-    left: 310,
-    top: 109,
-    width: "50%", /* Full width */
-    height: "70%",  /* Full height */
-    overflow: "auto",  /* Enable scroll if needed */
-    backgroundColor: "white",
-    margin: "0 auto",
-    borderRadius: "10px"
-
-  }
-
-
-  toggleSignInModal = () => {
-    this.state.signInModalOpen ? this.setState({
-      currentSignInModalStyle: this.closedModalStyle
-    }) : this.setState({
-      currentSignInModalStyle: this.openedModalStyle,
-      currentSignUpModalStyle: this.closedModalStyle
-    })
-    this.setState({
-      signInModalOpen: !this.state.signInModalOpen,
-      signUpModalOpen: !this.state.signUpModalOpen
-    })
-  }
-
-  toggleSignUpModal = () => {
-    this.state.signUpModalOpen ? this.setState({
-      currentSignUpModalStyle: this.closedModalStyle
-    }) : this.setState({
-      currentSignUpModalStyle: this.openedModalStyle,
-      currentSignInModalStyle: this.closedModalStyle
-    })
-    this.setState({
-      signUpModalOpen: !this.state.signUpModalOpen,
-      signInModalOpen: !this.state.signInModalOpen
-    })
-  }
   componentDidMount() {
-    this.setState({
-      currentSignInModalStyle: this.closedModalStyle, 
-      currentSignUpModalStyle: this.closedModalStyle, 
-      signUpModalOpen: false,
-      signInModalOpen: false
-    })
+
+  }
+
+  toggleSignInModal1 = () => {
+    let styles1 = {
+      display: "block"
+    }
+    
+    if(this.state.styles1.display === "none"){
+      this.setState({styles1})
+    }
+  }
+
+  closeSignInModal1 = () => {
+    let styles1= {
+      display: "none"
+    }
+    if(this.state.styles1.display === "block"){
+      this.setState({styles1})
+    }
+  }
+
+  toggleSignInModal2 = () => {
+    let styles2 = {
+      display: "block"
+    }
+    
+    if(this.state.styles2.display === "none"){
+      this.setState({styles2})
+    }
+  }
+
+  closeSignInModal2 = () => {
+    let styles2= {
+      display: "none"
+    }
+    if(this.state.styles2.display === "block"){
+      this.setState({styles2})
+    }
   }
 
   handleInputChange = (event) => {
@@ -127,40 +99,39 @@ class Home extends React.Component {
   render() {
 
     return (
-        <div className="container">
+        <div>
             
-            <button id="openModal" onClick={this.toggleSignInModal}>Sign In</button>
-      <div className="row">
-        <div className="col-md-7">
-        <div className="jumbotron shadow-lg  p-3 mb-5">
-            <h5 className = "currentPage">1 <span className="blue">Created</span> <span className="purple">For
-            </span> (<span className="developer">Developers</span>) <span className="purple">By</span> [<span className="developer">Developers</span>] <span className="blue">=></span> &#123;</h5>
-            {/*first sentence*/}
-            <h6 className = "currentPage">2 <br></br>3 &#60;<span className="red">BitHub</span> <span className="yellow">is</span> the latest <span className="blue">tool</span> <span className="purple">for</span> <span className="developer">developers</span> to <span className="blue">spread</span><br></br>4 <span className="lightBlue">knowledge</span> within their <span className="lightBlue">community</span>/&#62;; <br></br>5 <br></br>{/*second sentence*/}
-            6 &#60;<span className="blue">Here</span>, you can <span className="yellow">save</span> and <span className="yellow">share</span> your <span className="blue">best</span> <span className="lightBlue">code</span> <span className="purple">for</span> all <span className="purple">aspiring</span> <br></br>7 <span className="developer">developers</span> across the <span className="lightBlue">globe</span>/&#62;;<br></br>8 <br></br>{/*third sentence*/}
-            9 &#60;<span className="blue">You</span> will <span className="yellow">become</span> an <span className="purple">integral</span> <span className="lightBlue">part</span> in both the <br></br>10 <span className="lightBlue">enrichment</span> and the <span className="lightBlue">growth</span> of <span className="developer">developers</span> <span className="lightBlue">everywhere</span>/&#62;;<br></br>11 <br></br>12 &#125;;</h6>
-        </div>
-      </div>
-    </div>
-        <div id="modal" style={this.state.currentSignInModalStyle}>
-          <button id="closeModal" onClick={this.toggleSignInModal}>x</button>
-          <h1>Sign In</h1>
-          <input placeholder="Username"></input>
-          <input placeholder="Password"></input>
-          <button id="submit">Submit</button>
-          <a href="#modalSignUp" id="createLink" onClick={this.toggleSignUpModal}>Create an account </a>
-        </div>
-        <div id="modalSignUp" style={this.state.currentSignUpModalStyle}>
-          <button id="closeModal" onClick={this.toggleSignUpModal}>x</button>
-          <h1>Sign Up</h1>
-          <input placeholder="First Name"></input>
-          <input placeholder="Last Name"></input>
-          <input placeholder="Username"></input>
-          <input placeholder="Password"></input>
-          <button id="submit">Submit</button>
-          <a href="#modal" id="createLink" onClick={this.toggleSignInModal}>Sign In </a>
+            <button id="openModal" onClick={this.toggleSignInModal1}>Sign In</button>
+        
+        <div className="jumbotron">
+            <h1 className = "currentPage"><span className="blue">Created</span> <span className="purple">For</span> (<span className="developer">Developers</span>) <span className="purple">By</span> [<span className="developer">Developers</span>];</h1>
+            <br></br>
+            <h3 className = "currentPage">bitHub is the latest tool for developers to share knowledge with their community. 
+            Here, you can save your best code for all aspiring developers to use and learn from. 
+            You will become an integral part in both the enriching and the growth of developers everywhere.</h3>
         </div>
 
+        <div id="modal" style={this.state.styles1}>
+          <button id="closeModal" onClick={this.closeSignInModal1}>x</button>
+          <h1>Sign In</h1>
+          <form className="login-signup-form" onSubmit={()=>{this.handleLogIn()}}>
+            <input type="text" name="username" placeholder="Username" onChange={this.handleInputChange} autoFocus />
+            <input type="password" name="password" placeholder="Password" onChange={this.handleInputChange} />
+            <button className="success-button" id="submit" type="submit">Login</button>
+            <a href="#modalSignUp" id="createLink" onClick={()=>{this.closeSignInModal1();this.toggleSignInModal2()}}>Create an account</a>
+          </form>
+        </div>
+
+        <div id="modalSignUp" style={this.state.styles2}>
+          <button id="closeModalSignUp" onClick={this.closeSignInModal2}>x</button>
+          <h1>Sign Up</h1>
+          <form className="login-signup-form" onSubmit={()=>{this.handleSignUp()}}>
+            <input type="text" name="username" placeholder="Username" onChange={this.handleInputChange} autoFocus />
+            <input type="password" name="password" placeholder="Password" onChange={this.handleInputChange} />
+            <button className="success-button" type="submit">Sign Up</button>
+          </form>
+        </div>
+        
       </div>
     );
   }
