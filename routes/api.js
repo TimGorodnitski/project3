abe3404502ff84c03ef06762071d37acdd66eb59
Tim Gorodnitski <timgm3@gmail.com>
2018-11-02 11:31:17 -0700

    api working

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,18 @@
+const mongoose = require("mongoose");
 const router = require("express").Router();
 const Snippet = require("../client/models/Snippet");
-const User = require("../client/models/User");
+var User = mongoose.model('User');
+const passport = require('passport');
 
 
 router.post("/save", function(req, res) {
+
+  let d = new Date();
+  req.body.createdAt = d.toDateString();
+  const snippet = req.body;
+
   // as long as req.body matches what the model expects, this should insert into the database
- Snippet.create(req.body)
+ Snippet.create(snippet)
   .then(() => {
     res.json(true);
   })
@@ -53,6 +60,67 @@ router.delete("/delete/:id", function(req, res) {
     });
 });
 
+
+// Authentication Routing
+
+
+router.post('/login', passport.authenticate('local'), 
+  function(req, res) {
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+    res.json(req.user);
+  })
+
+
+router.post('/signup', (req, res, next) => {
+  console.log("/signup post: " + req.body);
+
+  req.checkBody('username', 'Username is required.').notEmpty();
+  req.checkBody('password', 'Password is required.').notEmpty();
+
+  req.getValidationResult().then((result) => {
+    if (result.isEmpty() === false) {
+      //bad
+      console.log('errors is empty', result.isEmpty());
+      result.array().forEach((error) => {
+        req.flash('error', error.msg);
+      });
+      res.redirect('/');
+    } else {
+
+      //good
+      const user = new User({
+        username: req.body.username,
+        password: req.body.password
+      });
+
+      user.save((err) => {
+        if(err) {
+          console.log('There was an error saving the user.', err);
+          if (err.message.indexOf('duplicate key error') > -1) {
+            req.flash('error', 'Username already in use.');
+          } else {
+            req.flash('error', 'There was a problem with your registration.');
+          };
+          console.log(req.flash);
+          res.redirect('/');
+        } else {
+          next();
+        }
+      });
+    }
+  })
+
+}
+, passport.authenticate('local', {
+  successRedirect: '/',
+})
+);
+
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
 
 
 module.exports = router;
