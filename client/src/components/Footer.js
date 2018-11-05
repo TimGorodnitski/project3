c3963fd85a7ef38765529f438a81d3328312af58
Tim Gorodnitski <timgm3@gmail.com>
2018-11-05 14:07:14 -0800

    homepage and footer styling

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,5 +1,4 @@
 import React from "react";
-import {Link} from "react-router-dom";
 import "./Footer.css";
 
 
@@ -25,11 +24,11 @@ class Footer extends React.Component {
         return (
             <div>
                 <footer className = "footer">
-                <a href="https://github.com/TimGorodnitski/project3" target="_blank">
+                <a href="https://github.com/TimGorodnitski/project3" target="_blank" rel="noopener noreferrer">
                <img className="gitLogo"   style={gitHubStyle} alt="gitHubLogo" src="https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png"></img> 
                </a>
                <div className="linkGit">
-                <a href="https://github.com/TimGorodnitski/project3" id="gitLink" target="_blank">GitHub</a>
+                <a href="https://github.com/TimGorodnitski/project3" id="gitLink" target="_blank" rel="noopener noreferrer">GitHub</a>
                 </div>
                 </footer>
             </div>
