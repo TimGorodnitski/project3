61c40e22c15afd9517f8de0305c9b86f9d0d98f6
Andy Tamez <AndyTamez@Andys-MacBook-Air.local>
2018-11-05 12:42:40 -0800

    modal in nav bar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,8 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import "./Header.css";
+import axios from "axios";
+import { relative } from "path";
 
 var navbarStyle = {
   display: "flex",
@@ -82,7 +84,101 @@ var loginStyle = {
 class Header extends React.Component {
 
   state={
+    styles1: {
+      display: "none",
+    },
+    styles2: {
+      display: "none"
+    },
+    username: "",
+    password: "",
+    currentUser: ""
   }
+
+  toggleSignInModal1 = () => {
+    let styles1 = {
+      display: "block"
+    }
+    
+    if(this.state.styles1.display === "none"){
+      this.setState({styles1})
+    }
+  }
+
+  closeSignInModal1 = () => {
+    let styles1= {
+      display: "none"
+    }
+    if(this.state.styles1.display === "block"){
+      this.setState({styles1})
+    }
+  }
+
+  toggleSignInModal2 = () => {
+    let styles2 = {
+      display: "block"
+    }
+    
+    if(this.state.styles2.display === "none"){
+      this.setState({styles2})
+    }
+  }
+
+  closeSignInModal2 = () => {
+    let styles2= {
+      display: "none"
+    }
+    if(this.state.styles2.display === "block"){
+      this.setState({styles2})
+    }
+  }
+
+  handleInputChange = (event) => {
+		// update any state property with the input value of the same name
+		this.setState({
+		  [event.target.name]: event.target.value
+		});
+	};
+
+  handleSignUp = (event) => {
+    event.preventDefault();
+    // send the entire state object to the back-end
+    console.log("Attempting signup with state: " + this.state.username + " " + this.state.password)
+		axios.post("/signup", this.state).then((response) => {
+			console.log(response.data)
+		  if (response.status === 200) {
+        let currentUser = this.state.username;
+
+        this.setState({currentUser}, () => this.props.passData("currentUser", this.state.currentUser));
+				alert("Signed up!")
+		  }
+		  // mongoose validation failed
+		  else {
+				alert("Error. Not Signed up.");
+      }
+      this.props.passData("loggedIn", true);
+		});
+  };
+  
+  handleLogIn = (event) => {
+    event.preventDefault();
+    // send the entire state object to the back-end
+    console.log("Attempting login with state: " + this.state.username + " " + this.state.password)
+		axios.post("/login", this.state).then((response) => {
+			console.log(response.data);
+		  if (response.data.username) {
+        var currentUser = response.data.username;
+
+        this.setState({currentUser}, () => this.props.passData("currentUser", this.state.currentUser));
+        alert("Logged In!");
+		  }
+		  // mongoose validation failed
+		  else {
+				alert("Error. Not Logged in.");
+      }
+      this.props.passData("loggedIn", true);
+		});
+  };
 
 
   componentDidMount() {
@@ -109,9 +205,38 @@ class Header extends React.Component {
                 <Link to="/new" id="newsnippetLink" className="nav-link navLink" style={LinkStyle}>New Snippet</Link>
 
                 <Link to="/profile" id="mysnippetsLink" className="nav-link navLink" style={linkstyles}>My Snippets</Link>
+
                 
                 <img className = "loginIMG" style={loginStyle} alt="profile" href="/profile" src="https://static.thenounproject.com/png/23665-200.png"></img>
                 <span className="userName">{this.props.currentUser}</span>
+
+            <button id="openModal" onClick={this.toggleSignInModal1}>Sign In</button>
+
+
+                        <div id="modal" style={this.state.styles1}>
+          <button id="closeModal" onClick={this.closeSignInModal1} type="button" className="close" aria-label="Close">
+           <span aria-hidden="true">&times;</span>
+          </button>
+          <h1>Sign In</h1>
+          <form className="login-signup-form" onSubmit={(event)=>{this.handleLogIn(event)}}>
+            <input type="text" name="username" placeholder="Username" onChange={this.handleInputChange} autoFocus /><br></br>
+            <input type="password" name="password" placeholder="Password" onChange={this.handleInputChange} /><br></br>
+            <button className="success-button" id="submit" type="submit">Login</button> <br></br>
+            <a href="#modalSignUp" id="createLink" onClick={()=>{this.closeSignInModal1();this.toggleSignInModal2()}}>Create an account</a>
+          </form>
+        </div>
+
+        <div id="modalSignUp" style={this.state.styles2}>
+          <button id="closeModalSignUp" onClick={this.closeSignInModal2} type="button" className="close" aria-label="Close">
+           <span aria-hidden="true">&times;</span>
+          </button>
+          <h1>Sign Up</h1>
+          <form className="login-signup-form" onSubmit={(event)=>{this.handleSignUp(event)}}>
+            <input type="text" name="username" placeholder="Username" onChange={this.handleInputChange} autoFocus /><br></br>
+            <input type="password" name="password" placeholder="Password" onChange={this.handleInputChange} /><br></br>
+            <button className="success-button" type="submit">Sign Up</button>
+          </form>
+        </div>
 
               </div>
             </header>
@@ -120,7 +245,5 @@ class Header extends React.Component {
       )
   }
 }
-
-
 
 export default Header;
